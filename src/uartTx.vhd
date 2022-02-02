4b99bb28c25af3a0a6617433dcf8b2924a402e0e
bahadiryildirimeem <bahadiryildirim15@gmail.com>
2022-02-03 00:14:19 +0300

    I forgot something xd

--- src/uartTx.vhd
+++ src/uartTx.vhd
@@ -5,15 +5,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- baudrate calculation:
 -- baudrate : CLK_FREQ / DIVISOR
 entity uartTx is
-    Generic( CLK_FREQ : INTEGER := 100_000_000;
+    Generic(CLK_FREQ : INTEGER := 100_000_000;
 	         DIV_DPT  : INTEGER := 12);
-    Port ( 	CLK      : in  STD_LOGIC;
-			DATI     : in  STD_LOGIC_VECTOR(7 downto 0);
-			STOP_BIT : in  STD_LOGIC_VECTOR(1 downto 0);
-			DIVISOR  : in  STD_LOGIC_VECTOR(DIV_DPT - 1 downto 0);
-			SEND     : in  STD_LOGIC;
-			BUSY     : out STD_LOGIC;
-			TX       : out STD_LOGIC);
+    Port ( CLK      : in  STD_LOGIC;
+           DATI     : in  STD_LOGIC_VECTOR(7 downto 0);
+			  STOP_BIT : in  STD_LOGIC_VECTOR(1 downto 0);
+			  DIVISOR  : in  STD_LOGIC_VECTOR(DIV_DPT - 1 downto 0);
+           SEND     : in  STD_LOGIC;
+           BUSY     : out STD_LOGIC;
+           TX       : out STD_LOGIC);
 end uartTx;
 
 architecture Behavioral of uartTx is
@@ -44,7 +44,7 @@ begin
    if(rising_edge(CLK)) then
 	   case states is
 		   when IDLE =>
-				if(SEND = '1') then
+			   if(SEND = '1') then
 				   baudCnt <= 0;
 					baudCntLim <= divQ - 1;
 					bitCnt <= 0;
@@ -64,10 +64,15 @@ begin
 				   states <= DATA_SENDING;
 				end if;
 			when DATA_SENDING =>
-			    if(bitCnt = 8) then
-				   baudCnt <= 0;
-				   bitCnt <= 0;
-				   states <= STOP_SEND;
+			   if(bitCnt = 8) then
+					if(stopBitQ = 0) then
+				      busyQ <= '0';
+					   states <= IDLE;
+					else
+					   baudCnt <= 0;
+				      bitCnt <= 0;
+				      states <= STOP_SEND;
+					end if;
 				else
 				   txQ <= datiQ(bitCnt);
 				   baudCnt <= baudCnt + 1;
@@ -77,15 +82,15 @@ begin
 					bitCnt <= bitCnt + 1;
 				end if;
 			when STOP_SEND => 
-			    txQ <= '1';
-			    if(bitCnt = stopBitQ) then
+			   txQ <= '1';
+			   if(bitCnt = stopBitQ) then
 				   busyQ <= '0';
 					states <= IDLE;
 				else
 				   baudCnt <= baudCnt + 1;
 				end if;
 				if(baudCnt = baudCntLim) then
-				    baudCnt <= 0;
+				   baudCnt <= 0;
 					bitCnt <= bitCnt + 1;
 				end if;
 		end case;
